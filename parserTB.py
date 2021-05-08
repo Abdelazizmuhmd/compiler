7d17fbeb69cf078297c6af7adeda63469ce3477b
Abdelazizmuhmd <41162973+Abdelazizmuhmd@users.noreply.github.com>
2021-05-08 19:26:39 +0200

    updated parser

--- parserTB.py
+++ parserTB.py
@@ -18,34 +18,257 @@ class Parser():
     def parse(self):
         #1
         @self.pg.production('Program : Start_Statement ClassDeclaration Delimiter End_Statement Access_Operator')
+        def Program(P):
+            print("matched Program")
+            return Print(p[1])
         @self.pg.production('Program : Comment Delimiter using_command')
         def Program(P):
             print("matched Program")
+            return Print(p[2])
 
         #2
         @self.pg.production('ClassDeclaration : Class Delimiter IDENTIFIER Braces Class_Implementation Braces')
+        def ClassDeclaration(P):
+            print("matched ClassDeclaration")
+            return Print(p[4])
         @self.pg.production('ClassDeclaration : Class Delimiter IDENTIFIER Delimiter Inheritance Braces Class_Implementation Braces')
         def ClassDeclaration(P):
             print("matched ClassDeclaration")
+            return Print(p[6])
 
         #3
         @self.pg.production('Class_Implementation : VarDeclaration Class_Implementation')
-        @self.pg.production('Class_Implementation : MethodDeclaration Class_Implementation')
-        @self.pg.production('Class_Implementation : Comment Class_Implementation')
-        @self.pg.production('Class_Implementation : Func_Call Class_Implementation')
-        #@self.pg.production('empty')
         def Class_Implementation(P):
             print("matched Class_Implementation")
+            return Print(p[0],p[1])
+        @self.pg.production('Class_Implementation : MethodDeclaration Class_Implementation')
+        def Class_Implementation(P):
+            print("matched Class_Implementation")
+            return Print(p[0], p[1])
+        @self.pg.production('Class_Implementation : Comment Class_Implementation')
+        def Class_Implementation(P):
+            print("matched Class_Implementation")
+            return Print(p[1])
+        @self.pg.production('Class_Implementation : Func_Call Class_Implementation')
+        def Class_Implementation(P):
+            print("matched Class_Implementation")
+            return Print(p[0],p[1])
+
+
+
         #4
-        @self.pg.production('MethodDeclaration : Func Decl Delimiter')
-        @self.pg.production('MethodDeclaration : Func Decl Braces VarDeclaration Statements Braces')
+        @self.pg.production('MethodDeclaration : Func_Decl Delimiter')
         def MethodDeclaration(P):
             print("matched MethodDeclaration")
+            return Print(p[0])
+        @self.pg.production('MethodDeclaration : Func_Decl Braces VarDeclaration Statements Braces')
+        def MethodDeclaration(P):
+            print("matched MethodDeclaration")
+            return Print(p[0],p[2],p[3])
 
         #5
-        @self.pg.production('Func Decl : Type Delimiter IDENTIFIER Angle_brackets ParameterList Angle_brackets')
-        def FuncDecl(P):
-            print("matched Func Decl")
+        @self.pg.production('Func_Decl : Type Delimiter IDENTIFIER Angle_brackets ParameterList Angle_brackets')
+        def Func_Decl(P):
+            print("matched Func_Decl")
+            return Print(p[0],p[4])
+
+
+
+        #7
+        @self.pg.production('ParameterList : Non-Empty_List')
+        def ParameterList(P):
+            print("matched ParameterList")
+            return Print(P[0])
+
+        #8
+        @self.pg.production('Non-Empty_List : Type Delimiter IDENTIFIER')
+        def Non-Empty_List(P):
+            print("matched Non-Empty_List")
+            Print(p[0])
+        #idk about comma , no end
+        @self.pg.production('Non-Empty_List : Non-Empty_List , Type Delimiter IDENTIFIER')
+        def Non-Empty_List(P):
+            print("matched Non-Empty_List")
+            Print(p[0],p[1])
+
+        #9
+        @self.pg.production('VarDeclaration : empty')
+        @self.pg.production('VarDeclaration : Type Delimiter ID_List Delimiter VarDeclaration')
+        def VarDeclaration(P):
+            print("matched VarDeclaration")
+            Print(p[0],p[2],p[4])
+        #10
+        #idk about comma
+        @self.pg.production('ID_List : ID_List , IDENTIFIER')
+        def ID_List(P):
+            print("matched ID_List")
+            Print(p[0])
+        #11
+        @self.pg.production('Statements : Statement Statements')
+        def Statements(P):
+            print("matched Statements")
+            Print(p[0],p[1])
+
+        #12
+
+        @self.pg.production('Statement : WhetherDo_Statement')
+        def Statement(P):
+            print("matched Statement")
+            Print(p[0])
+
+        #18
+        @self.pg.production('WhetherDo_Statement : CONDITION Angle_brackets Condition_Expression Angle_brackets Block_Statements CONDITION Statement')
+        def WhetherDo_Statement(P):
+            print("matched WhetherDo_Statement")
+            Print(p[2],p[4],p[6])
+
+
+        @self.pg.production('Statement : Assignment')
+        def Statement(P):
+            print("matched Statement")
+            Print(p[0])
+
+        @self.pg.production('Statement : RingWhen_Statement')
+        def Statement(P):
+            print("matched Statement")
+            Print(p[0])
+        @self.pg.production('Statement : BackedValue_Statement')
+        def Statement(P):
+            print("matched Statement")
+            Print(p[0])
+        @self.pg.production('Statement : terminatethis_Statement')
+        def Statement(P):
+            print("matched Statement")
+            Print(p[0])
+        #idk about them
+        @self.pg.production('Statement : write Angle_brackets Expression Angle_brackets Delimiter')
+        def Statement(P):
+             print("matched Statement")
+             Print(p[2])
+         #should moved down
+         @self.pg.production('Statement : read Angle_brackets IDENTIFIER Angle_brackets Delimiter')
+         def Statement(P):
+             print("matched Statement")
+
+        #13
+        @self.pg.production('Assignment : VarDeclaration Assignment_operator Expression Delimiter')
+        def Assignment(P):
+            print("matched Assignment")
+            Print(p[0],p[2])
+
+        #14
+        @self.pg.production('Func_Call : IDENTIFIER Angle_brackets Argument_List Angle_brackets Delimiter')
+        def Func_Call(P):
+            print("matched Func_Call")
+            Print(p[2])
+
+        #15
+        @self.pg.production('Argument_List : NonEmpty_Argument_List')
+        def Argument_List(P):
+            print("matched Argument_List")
+            Print(p[0])
+
+        #16
+        #idk about comma
+        @self.pg.production('NonEmpty_Argument_List : NonEmpty_Argument_List , Expression')
+        def onEmpty_Argument_List(P):
+            print("matched onEmpty_Argument_List")
+            Print(p[0])
+        @self.pg.production('NonEmpty_Argument_List : Expression')
+        def onEmpty_Argument_List(P):
+            print("matched onEmpty_Argument_List")
+            Print(p[0])
+
+        #23
+        @self.pg.production('RingWhen_Statement : Loop Angle_brackets Condition_Expression Angle_brackets Block_Statements')
+        def RingWhen_Statement(P):
+            print("matched  RingWhen_Statement")
+            Print(p[2],p[4])
+
+        #17
+        @self.pg.production('Block_Statements : Braces Statements Braces')
+        def BlockS_tatements(P):
+            print("matched Block_Statements")
+            Print(p[1])
+
+
+
+        #19
+        @self.pg.production('Condition_Expression : Condition')
+        def Condition_Expression(P):
+            print("matched  Condition_Expression")
+            Print(p[0])
+        @self.pg.production('Condition_Expression : Condition Condition_Op Condition')
+        def Condition_Expression(P):
+            print("matched  Condition_Expression")
+            Print(p[0],p[1],p[2])
+
+
+
+        #21
+        @self.pg.production('Condition : Expression Comparison_Op Expression Constant Access_operator')
+        def Condition(P):
+                print("matched  Condition")
+                Print(p[0],p[1],p[2])
+
+
+
+
+        #24
+        #extra space after backed value
+        @self.pg.production('BackedValue_Statement : Return Delimiter Expression Delimiter')
+        def BackedValue_Statement(P):
+            print("matched  BackedValue_Statement")
+            Print(p[2])
+        @self.pg.production('BackedValue_Statement : Return Delimiter IDENTIFIER Expression Delimiter')
+        def BackedValue_Statement(P):
+            print("matched  BackedValue_Statement")
+            Print(p[3])
+
+
+
+        #26
+        @self.pg.production('Expression : Expression Delimiter Add_Op Delimiter Term')
+        def Expression(P):
+            print("matched  Expression")
+            Print(p[0],p[2],p[4])
+        @self.pg.production('Expression : Term')
+        def Expression(P):
+            print("matched  Expression")
+            Print(p[0])
+
+
+
+        #28
+        @self.pg.production('Term : Factor')
+        def Term(P):
+            print("matched  Term")
+            Print(p[0])
+        @self.pg.production('Term : Term Delimiter Mul_Op Delimiter Factor')
+        def Term(P):
+            print("matched  Term")
+            Pirnt(p[0],p[2],p[4])
+
+        #32
+        @self.pg.production('using_command :  Inclusion Angle_brackets F_name Access Operator txt Angle_brackets Delimiter')
+        def using_command(P):
+            print("matched  using_command")
+            Print(p[2])
+
+        #33
+        #what is  STR
+        @self.pg.production('F_name : String')
+        def F_name(P):
+            print("matched  F_name")
+
+        #31
+        #what is changed STR to String
+        @self.pg.production('Comment : Comment String Comment')
+        @self.pg.production('Comment : Comment String Comment')
+        @self.pg.production('Comment : Comment String')
+        def Comment(P):
+            print("matched  Comment")
+
 
         #6
         @self.pg.production('Type : Integer')
@@ -59,133 +282,57 @@ class Parser():
             print("matched Type")
 
         #7
-        @self.pg.production('ParameterList : empty')
+        #empty
+        @self.pg.production('ParameterList :')
+        def ParameterList(P):
+            print("matched ParameterList")
         @self.pg.production('ParameterList : NoneValue')
-        @self.pg.production('ParameterList : Non-Empty List')
         def ParameterList(P):
             print("matched ParameterList")
 
-        #8
-        @self.pg.production('Non-Empty List : Type Delimiter IDENTIFIER')
-        @self.pg.production('Non-Empty List : Non-Empty List , Type Delimiter IDENTIFIER')
-        def NonEmptyList(P):
-            print("matched Non-Empty List")
-
         #9
-        #@self.pg.production('VarDeclaration : empty')
-        @self.pg.production('VarDeclaration : Type Delimiter ID_List Delimiter VarDeclaration')
+        @self.pg.production('VarDeclaration :')
         def VarDeclaration(P):
             print("matched VarDeclaration")
 
+        #3
+        #empty
+        @self.pg.production('Class_Implementation :')
+        def Class_Implementation(P):
+            print("matched Class_Implementation")
+
         #10
         @self.pg.production('ID_List : IDENTIFIER')
-        @self.pg.production('ID_List : ID_List , IDENTIFIER')
         def ID_List(P):
             print("matched ID_List")
 
         #11
-        @self.pg.production('Statements : empty')
-        @self.pg.production('Statements : Statement Statements')
+        @self.pg.production('Statements :')
         def Statements(P):
             print("matched Statements")
-
-        #12
-        @self.pg.production('Statement : Assignment')
-        @self.pg.production('Statement : WhetherDo_Statement')
-        @self.pg.production('Statement : RingWhen_Statement')
-        @self.pg.production('Statement : BackedValue_Statement')
-        @self.pg.production('Statement : terminatethis_Statement')
-        @self.pg.production('Statement : read Angle_brackets IDENTIFIER Angle_brackets Delimiter')
-        @self.pg.production('Statement : write Angle_brackets Expression Angle_brackets Delimiter')
-        def Statement(P):
-             print("matched Statement")
-
-        #13
-        @self.pg.production('Assignment : VarDeclaration Assignment_operator Expression Delimiter')
-        def Assignment(P):
-            print("matched Assignment")
-
-        #14
-        @self.pg.production('Func_Call : IDENTIFIER Angle_brackets Argument_List Angle_brackets Delimiter')
-        def Func_Call(P):
-            print("matched Func_Call")
-
         #15
-        @self.pg.production('Argument_List : empty')
-        @self.pg.production('Argument_List : NonEmpty_Argument_List')
-        def Argument_List(P):
-            print("matched Argument_List")
+        @self.pg.production('Argument_List :')
 
-        #16
-        @self.pg.production('NonEmpty_Argument_List : Expression')
-        @self.pg.production('NonEmpty_Argument_List : NonEmpty_Argument_List , Expression')
-        def onEmpty_Argument_List(P):
-            print("matched onEmpty_Argument_List")
-        #17
-        @self.pg.production('Block Statements : Braces Statements Braces')
-        def BlockStatements(P):
-            print("matched Block Statements")
-
-        #18
-        @self.pg.production('WhetherDo_Statement : CONDITION Angle_brackets Condition_Expression Angle_brackets Block Statements CONDITION Statement')
-        def WhetherDo_Statement(P):
-            print("matched WhetherDo_Statement")
-
-        #19
-        @self.pg.production('Condition_Expression : Condition')
-        @self.pg.production('Condition_Expression : Condition Condition_Op Condition')
-        def Condition_Expression(P):
-            print("matched  Condition_Expression")
 
         #20
-        # and | or not not token nor rule but there is && || which is logic operators
         @self.pg.production('Condition_Op : Logic operators')
         def Condition_Op(P):
             print("matched  Condition_Op")
-
-        #21
-        @self.pg.production('Condition : Expression Comparison_Op Expression Constant Access_operator')
-        def Condition(P):
-                print("matched  Condition")
 
         #22
         @self.pg.production('Comparison_Op : relational_operators')
         def Comparison_Op(P):
             print("matched  Comparison_Op")
-
-        #23
-        @self.pg.production('RingWhen_Statement : Loop Angle_brackets Condition_Expression Angle_brackets Block Statements')
-        def RingWhen_Statement(P):
-            print("matched  RingWhen_Statement")
-
-        #24
-        #extra space after backed value
-        @self.pg.production('BackedValue_Statement : Return Delimiter Expression Delimiter')
-        @self.pg.production('BackedValue_Statement : Return Delimiter ID Expression Delimiter')
-        def BackedValue_Statement(P):
-            print("matched  BackedValue_Statement")
-
         #25
         @self.pg.production('terminatethis_Statement : Break Delimiter')
         def terminatethis_Statement(P):
             print("matched  terminatethis_Statement")
 
-        #26
-        @self.pg.production('Expression : Term')
-        @self.pg.production('Expression : Expression Delimiter Add_Op Delimiter Term')
-        def Expression(P):
-            print("matched  Expression")
 
         #27
         @self.pg.production('Add_Op : Arithmetic_Operation')
         def Add_Op(P):
             print("matched  Add_Op")
-
-        #28
-        @self.pg.production('Term : Factor')
-        @self.pg.production('Term : Term Delimiter Mul_Op Delimiter Factor')
-        def Term(P):
-            print("matched  Term")
 
         #29
         @self.pg.production('Mul_Op : Arithmetic_Operation')
@@ -194,30 +341,13 @@ class Parser():
 
         #30
         @self.pg.production('Factor : IDENTIFIER')
-        @self.pg.production('Factor : Number')
         def Factor(P):
             print("matched  Factor")
 
-
-
-        #31
-        #what is STR
-        @self.pg.production('Comment : Comment String Comment')
-        @self.pg.production('Comment : Comment String Comment')
-        @self.pg.production('Comment : Comment String')
-        def Comment(P):
-            print("matched  Comment")
-
-        #32
-        @self.pg.production('using_command :  Inclusion Angle_brackets F_name Access Operator txt Angle_brackets Delimiter')
-        def using_command(P):
-            print("matched  using_command")
-
-        #33
-        #what is  STR
-        @self.pg.production('F_name : String')
-        def F_name(P):
-            print("matched  F_name")
+        # idk about number so i made it constant
+        @self.pg.production('Factor : Constant')
+        def Factor(P):
+            print("matched  Factor")
 
         @self.pg.error
         def error_handle(token):
